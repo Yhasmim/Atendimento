dfbf6c4198189d7e1ff593e8f052862c682cf651
Yhasmim <55556122+Yhasmim@users.noreply.github.com>
2019-10-04 19:55:01 -0300

    Add files via upload

---
+++ projAtendimento/projAtendimento/Models/Senha.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projAtendimento.Models
+{
+    class Senha
+    {
+        private int id;
+        private DateTime dataGerac;
+        private DateTime horaGerac;
+        private DateTime dataAtend;
+        private DateTime horaAtend;
+
+        public DateTime DataGerac { get => dataGerac; set => dataGerac = value; }
+        public DateTime HoraGerac { get => horaGerac; set => horaGerac = value; }
+        public DateTime DataAtend { get => dataAtend; set => dataAtend = value; }
+        public DateTime HoraAtend { get => horaAtend; set => horaAtend = value; }
+
+        public Senha(int id)
+        {
+            this.id = id;
+            this.dataGerac = DateTime.Now;
+            this.horaGerac = DateTime.Now;
+        }
+
+        public string dadosParciais()
+        {
+            return id + " - " + DataGerac.ToString("dd/MM/yyyy") + " - " + HoraGerac.ToString("HH:mm");
+        }
+
+        public string dadosCompletos()
+        {
+            return id + " - " + DataGerac.ToString("dd/MM/yyyy") + " - " + HoraGerac.ToString("HH:mm") + " - " + DataAtend.ToString("dd/MM/yyyy") + " - " + HoraAtend.ToString("HH:MM");
+        }
+        
+    }
+}
